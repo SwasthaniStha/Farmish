d8716d4a423268e1f99546822067092edd14ec43
Pranjali Pudasaini <119116985+pranjalipudasaini@users.noreply.github.com>
2024-05-07 19:01:29 +0545

    Delete farmerdashboard directory

--- farmerdashboard/product/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Product
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        exclude = ['available_quantity', 'sold_quantity']
-
